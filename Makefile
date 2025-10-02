14103ad2429cf602fe67da8304df9588e126933d
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-10-02 09:05:01 +0000

    Initiate project environments and requirements with uv

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+		UV_HTTP_TIMEOUT=120 uv add -r requirements.txt
+
+
+	
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install UV environment and add requirements
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# initialize uv environment
+uv init
